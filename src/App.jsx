5266a87cf13fc32d7edbf5f0c6757129cc3fe6de
DavidLamasCEV <david_lamas_m-hncin1ma2122@cev.com>
2025-11-19 14:42:07 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Items />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
     </>
-  )
+  );
+}
+
+function Items(){
+  return (
+    <div>
+      <h1>Items</h1>
+      <p>Aquí están los items.</p>
+    </div>
+  )  
+}
+
+function Profile(){
+  return (
+    <div>
+      <h1>Perfil</h1>
+      <p>Datos del usuario</p>
+    </div>
+  )  
+}
+
+function Contact(){
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Formulario para contactar</p>
+    </div>
+  )  
+}
+
+function AboutUs(){
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Página con info estática</p>
+    </div>
+  )  
 }
 
 export default App
